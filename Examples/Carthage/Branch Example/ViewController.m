124e28d87d01770443a224706a10c870f1871a56
Sam Dozor <sdozor@mparticle.com>
2018-01-22 16:18:53 -0500

    Add sample apps

---
+++ Examples/Carthage/Branch Example/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  mParticle Branch Example
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
